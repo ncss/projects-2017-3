a9736cf791258d3b02361a02ec5aab643206bc71
algoth3214 <lucasladyman@gmail.com>
2017-01-09 17:38:02 +1100

    signup handler, and page

--- server.py
+++ server.py
@@ -17,7 +17,17 @@ def get_template(filename):
 def index_handler(request):
     request.write("hello there")
 
+def signup_handler(request):
+    render_file(request, 'signup.html', {})
+    ident = request.get_field('id')
+    username = request.get_field('username')
+    email = request.get_field('email')
+    password = request.get_field('password')
+    doc = request.get_field('doc')
+    gender = request.get_field('gender')
+    dob = request.get_field('dob')
 
 server = Server()
 server.register(r'/', index_handler)
+server.register(r'/signup', signup_handler)
 server.run()
