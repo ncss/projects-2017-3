ee600df3b0eaa35395f838637a7f743ab1e4921b
Jack <jackywathy24@gmail.com>
2017-01-09 16:51:26 +1100

    added render_file function which renders a file

--- server.py
+++ server.py
@@ -3,9 +3,19 @@ from tornado.ncss import Server, ncssbook_log
 
 TEMPLATE_DIR = 'templates'
 
+
+def render_file(response, filename:str, variables) -> None:
+    """Renders the filename replaceing {name} with keys in variables"""
+    response.write(get_template(filename).format(**variables))
+
+
 def get_template(filename):
+    """
+    Gets the template from TEMPLATE_DIR with name filename
+    """
     with open(os.path.join(TEMPLATE_DIR, filename)) as f:
         return f.read()
+
 
 def index_handler(request):
     request.write("hello there")
@@ -25,7 +35,7 @@ def test_reader(request):
     lname = request.get_field('lname')
     food = request.get_field('food')
     user = {'fname': fname, 'lname': lname, 'food': food}
-    request.write(template.format(**user))
+    render_file(request, 'test.html', user)
 
 server = Server()
 server.register(r'/', index_handler)
