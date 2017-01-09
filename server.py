1c13659da5dddc060a80d9bf8adb4409cf2d806c
Dylan Sanusi-Goh <dsg2806@yahoo.com.au>
2017-01-09 15:46:35 +1100

    Updated index_handler to handle URL parameter

--- server.py
+++ server.py
@@ -1,7 +1,14 @@
 from tornado.ncss import Server, ncssbook_log
 
 def index_handler(request):
-    request.write("hello world")
+    request.write("hello there")
+
+    param_name = request.get_field("name")
+    if param_name != None:
+        request.write("<br>")
+        request.write(param_name)
+
+    request.write("!")
 def book_handler(request, book_id):
     request.write(book_id)
 
