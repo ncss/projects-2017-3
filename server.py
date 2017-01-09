44c90f01106728a4beaf8ba8e6e779c72acd152f
xtilda <matilda.teitzel@optusnet.com.au>
2017-01-09 16:24:33 +1100

    Introduced rendering + show url parameters in HTML

--- server.py
+++ server.py
@@ -1,4 +1,11 @@
+import os
 from tornado.ncss import Server, ncssbook_log
+
+TEMPLATE_DIR = 'templates'
+
+def get_template(filename):
+    with open(os.path.join(TEMPLATE_DIR, filename)) as f:
+        return f.read()
 
 def index_handler(request):
     request.write("hello there")
@@ -12,8 +19,16 @@ def index_handler(request):
 def book_handler(request, book_id):
     request.write(book_id)
 
+def test_reader(request):
+    template = get_template('test.html')
+    fname = request.get_field('fname')
+    lname = request.get_field('lname')
+    food = request.get_field('food')
+    user = {'fname': fname, 'lname': lname, 'food': food}
+    request.write(template.format(**user))
 
 server = Server()
 server.register(r'/', index_handler)
 server.register(r'/book/(\d+)/?', book_handler)
+server.register(r'/test', test_reader)
 server.run()
