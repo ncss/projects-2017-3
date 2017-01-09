dae823d751b6899e0e1837bfde5265d7f598aebe
Jack <jackywathy24@gmail.com>
2017-01-09 17:28:05 +1100

    added requires_login decorator

--- server.py
+++ server.py
@@ -3,9 +3,11 @@ from tornado.ncss import Server, ncssbook_log
 
 TEMPLATE_DIR = 'templates'
 
+
 def render_file(response, filename:str, variables) -> None:
     """Renders the filename replaceing {name} with keys in variables"""
     response.write(get_template(filename).format(**variables))
+
 
 def get_template(filename):
     """
@@ -18,6 +20,14 @@ def index_handler(request):
     request.write("hello there")
 
 
+def ask_handler(request):
+    name = request.get_field("name")
+    render_file(request, "ask.html", {'username':str(name)})
+
+def ask_handler_put(request):
+    ...
+
 server = Server()
 server.register(r'/', index_handler)
+server.register(r'/ask', ask_handler)
 server.run()
