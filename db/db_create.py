81f302dcef6a21f30f231c3fc1119a5dc3a1001d
Chraser <yapkaymen@gmail.com>
2017-01-10 11:43:41 +1100

    Finished scheme. Starting classes

--- db/db_create.py
+++ db/db_create.py
@@ -6,9 +6,9 @@ with sqlite3.connect('db.db') as conn:
     CREATE TABLE users
     (username TEXT NOT NULL,
     email TEXT NOT NULL,
-    UNIQUE INTEGER PRIMARY KEY(id),
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     password TEXT NOT NULL,
-    creation_date TEXT NOT NULL,
+    datetime TEXT NOT NULL,
     gender TEXT,
     dob TEXT,
     bio TEXT,
@@ -17,22 +17,31 @@ with sqlite3.connect('db.db') as conn:
     '''
     )
 
+
     cur.execute(
     '''
-    CREATE TABLE photos
-    (UNIQUE INTEGER PRIMARY KEY(id),
-    file_name TEXT NOT NULL,
-    FOREIGN KEY(user_id) REFERENCES users(id),
-    FOREIGN KEY(post_id) REFERENCES posts(id), photo_date NOT NULL)
+    CREATE TABLE posts
+    (id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER NOT NULL,
+    description TEXT,
+    title TEXT NOT NULL,
+    post_date TEXT NOT NULL,
+    FOREIGN KEY(user_id) REFERENCES users(id))
 
     '''
     )
 
     cur.execute(
     '''
-    CREATE TABLE posts
-    (UNIQUE PRIMARY KEY(id), FOREIGN KEY(user_id) REFERENCES users(id),
-     description, title NOT NULL, post_date NOT NULL)
+    CREATE TABLE photos
+    (id INTEGER PRIMARY KEY AUTOINCREMENT,
+    file_name TEXT NOT NULL,
+    user_id INTEGER NOT NULL,
+    post_id INTEGER NOT NULL,
+    photo_date TEXT NOT NULL,
+    FOREIGN KEY(user_id) REFERENCES users(id),
+    FOREIGN KEY(post_id) REFERENCES posts(id)
+    )
 
     '''
     )
@@ -40,9 +49,45 @@ with sqlite3.connect('db.db') as conn:
     cur.execute(
     '''
     CREATE TABLE comments
-    (UNIQUE PRIMARY KEY(id), FOREIGN KEY(user_id) REFERENCES users(id),
-    FOREIGN KEY(post_id) REFERENCES posts(id),
-    text NOT NULL, date NOT NULL, parent_id, score NOT NULL)
+    (id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER NOT NULL,
+    post_id INTEGER NOT NULL,
+    parent_id INTEGER NOT NULL,
+    text TEXT NOT NULL,
+    date TEXT NOT NULL,
+    score INTEGER NOT NULL,
+    FOREIGN KEY(parent_id) REFERENCES comments(id),
+    FOREIGN KEY(user_id) REFERENCES users(id),
+    FOREIGN KEY(post_id) REFERENCES posts(id))
 
     '''
     )
+
+with sqlite3.connect('db.db') as conn:
+    class User:
+
+        def __init__(self, username, email, password, datetime, gender, dob, bio, picture, nickname):
+            self.username = username
+            self.email = email
+            self.password = password
+            self.datetime = datetime
+            self.gender = gender
+            self.dob = dob
+            self.bio = bio
+            self.picture = picture
+            self.nickname = nickname
+
+
+        @staticmethod
+        def find(username):
+            cur = conn.cursor(
+            '''
+            SELECT *
+            FROM users
+            WHERE username = ? ''', (username,)
+            )
+            row = cur.fetchone()
+
+            if row is None:
+                raise UsernNotFOund('{} does not exist'.format(username))
+            return User(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[0], row[0], row[0], row[0])
