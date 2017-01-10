74f3447c7cd98bf27484b826137e7254115ad4eb
jack <jackywathy24@gmail.com>
2017-01-10 14:38:27 +1100

    Created a user object

--- auth.py
+++ auth.py
@@ -2,17 +2,35 @@ from typing import Callable
 
 
 USER_COOKIE = "current_user"
-ALL_USERS = {"jack", "lucas", "matilda", "dilan", 'julia'}
+ALL_USER = {} # username : USER (object)
+class User:
+    def __init__(self, id, username, pasw, nick, email, gend, dob, bio, date):
+        self.id = id
+        self.username = username
+        self.pasw = pasw
+        self.nick = nick
+        self.email = email
+        self.gend = gend
+        self.dob = dob
+        self.bio = bio
+        self.date = date
+        
+    def __str__(self):
+        return "id=%s, user=%s, pass=%s, nick=%s, email=%s, gend=%s, dob=%s, bio=%s, date=%s" % (self.id, self.username, self.pasw, self.nick, self.email, self.gend,
+                                                                                                 self.dob, self.bio, self.date
+                                                                                                 )
 
-def get_pass_hash(name):
+
+
+def check_cookie(response):
     #TODO USE A SQL DATABASE
-    #RETURN TRUE IF NAME IN ALL _USERS NOW
-    return
+
+    return False
 
 
 def authenticate_cookie(response):
     """Returns True if cookies can be authenicated"""
-    if response.get_secure_cookie("current_user") in ALL_USERS:
+    if check_cookie(response):
         return True
     return False
 
@@ -35,3 +53,7 @@ def requires_login(func: Callable) -> Callable:
 @requires_login
 def foo(response, *args, **kwargs):
     print(response)
+
+
+#x = User('1', 'jack', 'hash', 'jack', 'email', 'm', '241-25', 'joegjlqejfgjawdlfkjd', '2012391934')
+#print(x.id)
