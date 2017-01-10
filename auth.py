8d2233c6f0b8e65b5424f8f3e96ae71cf1510b10
Jack Jiang <jackywathy24@gmail.com>
2017-01-10 12:49:28 +1100

    updated authenticate_cookie

--- auth.py
+++ auth.py
@@ -1,9 +1,21 @@
 from typing import Callable
 
 
+USER_COOKIE = "current_user"
+ALL_USERS = {"jack", "lucas", "matilda", "dilan", 'julia'}
+
+def get_pass_hash(name):
+    #TODO USE A SQL DATABASE
+    #RETURN TRUE IF NAME IN ALL _USERS NOW
+    return
+
+
 def authenticate_cookie(response):
     """Returns True if cookies can be authenicated"""
-    return True
+    if response.get_secure_cookie("current_user") in ALL_USERS:
+        return True
+    return False
+
 
 
 def requires_login(func: Callable) -> Callable:
