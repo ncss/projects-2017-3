ebd29ccd447c4096ee9533e48d7061d63e9de3aa
Francis-Fulloffrenchpeople <lachlangrainger@yahoo.com.au>
2017-01-10 16:01:44 +1100

    Better posts

--- db/db_create.py
+++ db/db_create.py
@@ -129,34 +129,86 @@ with sqlite3.connect('db.db') as conn:
 
     class Post:
 
-        def __init__(self, id, user_id, description, title, date):
+        def __init__(self, id, user_id, description, title, date, files):
             self.id = id
             self.user_id = user_id
             self.description = description
             self.title = title
             self.date = date
+            self.files = files
 
         def find(id):
-            cur = conn.cursor(
+            cur = conn.execute(
             '''
             SELECT *
             FROM posts
             WHERE id = ? ''', (id,)
             )
-            row = cur.fetchone()
+            row1 = cur.fetchone()
 
-            if row is None:
+            if row1 is None:
                 raise PostNotFound('{} does not correspond to a post that exists.'.format(id))
-            return Post(row[0], row[1], row[2], row[3], row[4])
 
-        def create(user_id, description, title, date):
-            cur = conn.cursor(
+            cur = conn.execute(
+            '''
+            SELECT file_name
+            FROM photos
+            WHERE post_id = ? ''', (id,)
+            )
+            row2 = cur.fetchall()
+
+            return Post(row1[0], row1[1], row1[2], row1[3], row1[4], row2)
+
+        def add_photo(file_name, user_id, post_id, date):
+            cur = conn.execute(
+            '''
+            INSERT INTO photos (file_name, user_id, post_id, photo_date)
+            VALUES (?, ?, ?, ?); ''', (file_name, user_id, post_id, date)
+            )
+            conn.commit()
+            return (cur.lastrowid, file_name)
+
+        def delete_photo(post_id, file_name):
+            cur = conn.execute(
+            '''
+            DELETE FROM photos
+            WHERE post_id = ? AND file_name = ? ''' , (post_id, file_name)
+            )
+            conn.commit()
+
+        def create(user_id, description, title, date, photo_files):
+            cur = conn.execute(
             '''
             INSERT INTO posts (user_id, description, title, post_date)
-            VALUES (?, ?, ?, ?); ''', (user_id, description, title, date))
+            VALUES (?, ?, ?, ?); ''', (user_id, description, title, date)
+            )
+            id = cur.lastrowid
+            conn.commit()
 
-            return Post (cur.lastrowid, user_id, description, title, date)
+            for files in photo_files:
+                Post.add_photo(files, user_id, id, date)
+            return Post (id, user_id, description, title, date, photo_files)
 
-        def delete(id, user_id):
-            #   to do
-            pass
+        def delete(id):
+            cur = conn.excecute(
+            '''
+            DELETE FROM posts
+            WHERE id = ? ''' , (id,)
+            )
+            conn.commit()
+
+        def update(id, user_id, description, title, date, files):
+            cur = conn.excecute(
+            '''
+            UPDATE posts
+            SET description = ?,
+            title = ?,
+            WHERE id = ?
+            ''', (description, title, id)
+            )
+            conn.commit()
+            return Post(id, user_id, description, title, date, files)
+
+    Post.create(1, 'some text', 'some title', 'probally a string', ['file1', 'file2', 'file3'])
+    Post.find(1)
+    Post.delete_photo(1, 'file1')
