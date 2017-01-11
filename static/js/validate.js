26e91869181fd7886cc5edbc8ef9eaa27b55abf3
JackFrench <jackfrench45@gmail.com>
2017-01-11 16:10:57 +1100

    added validation.js file code so that userinput must be correct to submit form, some bugs still may be in the code.

--- static/js/validate.js
+++ static/js/validate.js
@@ -1,37 +1,51 @@
 $(document).ready(function(){
-/*
+
   $('.submit').click(function(evt){
-    validateForm();
+      return validateForm();
   });
-*/
+
 function isPresent($input) {
   if ($input.val().length < 8) {
-    $input.parent().find('.error').text('your is not long enough')
+    $input.parent().find('.error').text('this field is not long enough')
   }
+}
 
 function isValidEmail($input) {
+  debugger
+  var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+  if (!emailReg.test($input)) {
+    $input.parent().find('.error').text('the email address you entered is not a valide email address')
+  }
+}
 
+function isValidName($input) {
+  var nameReg = /^[A-Za-z0-9]+$/;
+  if (!nameReg.test($input)) {
+    $input.parent().find('.error').text('the name you entered for this field is invalid. Note that usernames and nicknames can only contain letters and numbers.')
+  }
 }
 
 
-}
 
 function validateForm() {
-
-    var nameReg = /^[A-Za-z]+$/
-    var usernameReg = /^[A-Za-z]+$/
-    var nicknameReg = /^[A-Za-z]+$/
-    var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
 
     var $username = $('.username')
     var $nickname = $('.nickname')
     var $password = $('.password')
     var $email = $('.email')
 
-    isPresent($username);
+    var validUsername = isPresent($username) && isValidName($username);
+    var validNickname = isPresent($nickname) && isValidName($username) ;
+    var validPassword = isPresent($password);
+    var validEmail = isPresent($email) && isValidEmail$(email);
 
+    if (validUsername && validNickname && validPassword && validEmail) {
+      return true;
+    }
 
-
+    else {
+      return false;
+    }
 
 }
 });
