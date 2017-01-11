2fed17385a80b31024ed6e13c8562d3c9ed980cc
JackFrench <jackfrench45@gmail.com>
2017-01-11 12:46:16 +1100

    changed class names in signup.html, created validate.js file to validate user input and added a jquery source code file

---
+++ static/js/validate.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+
+  $('.submit').click(function(evt){
+    evt.preventDefault();
+    validateForm();
+  });
+
+function isPresent($input) {
+  if ($input == "") {
+
+  }
+}
+
+function validateForm() {
+
+    var nameReg = /^[A-Za-z]+$/
+    var usernameReg = /^[A-Za-z]+$/
+    var nicknameReg = /^[A-Za-z]+$/
+    var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+
+    var username = $('.username').val();
+    var nickname = $('.nickname').val();
+    var password = $('.password').val();
+    var email = $('.email').val();
+
+    if (username.length < 8 ) {
+
+    }
+
+
+}
+});
