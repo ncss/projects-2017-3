b91917eb57229a0fe02d089785f92cfa426dc768
Lyneca <atraguliauniliantauthr@gmail.com>
2017-01-12 04:20:31 +1100

    added comments

--- backend/view.py
+++ backend/view.py
@@ -6,10 +6,21 @@ from auth import requires_login, authenticate_cookie
 
 
 def view_question_handler(request, question_id):
-    try:
-        post = db.Post.find(question_id)
-        post_info = {'user' : post.user_id, 'description' : post.description, 'question' : post.title, 'date' : post.date, 'file' : post.file, 'signed_in':authenticate_cookie(request), 'username': get_username(request)}
-        print(post.title)
-        request.write(render('view_question.html', post_info))
-    except:
-        request.write('Invalid Id')
+    # try:
+    post = db.Post.find(question_id)
+    post_info = {
+        'user': post.user_id,
+        'description': post.description,
+        'question': post.title,
+        'date': post.date,
+        'file': post.file,
+        'signed_in': authenticate_cookie(request),
+        'username': get_username(request),
+        'comments': post.all_comments(),
+        'user_ids': db.User.find_multiple(),
+    }
+    print(post.title)
+    request.write(render('view_question.html', post_info))
+    # except Exception as e:
+        # print(e.with_traceback)
+        # request.write('Invalid Id')
