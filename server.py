a37f9a86d76ec9218a152fa10f434d9ab0a59798
StiffKelly <bubble05>
2017-01-12 05:01:53 +1100

    WE GOT EM

--- server.py
+++ server.py
@@ -23,7 +23,7 @@ def index_handler(request):
 
 def aboutus_handler(request):
     request.write(render('aboutus.html', {}))
-
+def not_found_handler(request):
 def handle_list_users(request):
     request.write(render('list_users.html', {'users': db.User.find_multiple(), 'signed_in':authenticate_cookie(request), 'username': get_username(request)}))
 
@@ -39,4 +39,5 @@ server.register(r'/logout'      , user.signout_handler)
 server.register(r'/list_users'  , handle_list_users)
 server.register(r'/profile/(.+)', profile.view_handler, post=profile.view_handler_post)
 server.register(r'/aboutus'     , aboutus_handler)
+server.register(r'/.*'     , not_found_handler)
 server.run()
