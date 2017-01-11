2d4857b73687257520fafe3e7109bf603505921f
xtilda <matilda.teitzel@optusnet.com.au>
2017-01-12 01:59:17 +1100

    Working on file upload to DB

--- backend/view.py
+++ backend/view.py
@@ -6,10 +6,9 @@ from auth import requires_login, authenticate_cookie
 
 
 def view_question_handler(request, question_id):
-    try:
+    #try:
         post = db.Post.find(question_id)
-        post_info = {'user' : post.user_id, 'description' : post.description, 'question' : post.title, 'date' : post.date, 'file' : post.files, 'signed_in':authenticate_cookie(request)}
-        print(post.title)
+
+        post_info = {'user' : post.user_id, 'description' : post.description, 'question' : post.title, 'date' : post.date, 'file' : post.files[0][0], 'signed_in':authenticate_cookie(request)}
+        #print(post.files[0][0])
         request.write(render('view_question.html', post_info))
-    except:
-        request.write('Invalid Id')
