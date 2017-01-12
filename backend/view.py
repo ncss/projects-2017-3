5c4b93155d1a51e053e3db2a87ee1a112619f9f3
jack <jackywathy24@gmail.com>
2017-01-12 11:28:50 +1100

    added css fixed comment system + profile pictures in comments

--- backend/view.py
+++ backend/view.py
@@ -1,6 +1,7 @@
 from auth import requires_login
 from backend.common import *
 from template_engine import render
+from os import path
 from db import db_api as db
 from auth import requires_login, authenticate_cookie
 
@@ -20,7 +21,12 @@ def view_question_handler(request, question_id):
         'user_ids': db.User.find_multiple(),
         'photo_id': post.id,
     }
-    print(post.title)
+    for i in post_info['comments']:
+        curid = i.user_id
+        curuser = db.User.find(curid)
+        print(curuser.picture)
+        i.image = path.join("uploads", "user_image", curuser.picture) if curuser.picture else ""
+
     request.write(render('view_question.html', post_info))
     # except Exception as e:
         # print(e.with_traceback)
@@ -28,9 +34,18 @@ def view_question_handler(request, question_id):
 
 def comment_handler_post(request, photo_id):
     text = request.get_field('addComment')
-    if request.get_secure_cookie(USER_COOKIE):
-        user_cookie = request.get_secure_cookie(USER_COOKIE).decode()
-        print(user_cookie)
-        user = db.User.find_by_username(user_cookie)
-        print(user.create_comment(db.Post.find(photo_id), text, None))
-    # request.redirect('/')
+    print(text, 'gotten')
+    user_cookie = request.get_secure_cookie(USER_COOKIE)
+    if user_cookie is not None:
+        user_cookie = user_cookie.decode()
+        if db.User.find_by_username(user_cookie):
+            print(user_cookie)
+            user = db.User.find_by_username(user_cookie)
+            user.create_comment(db.Post.find(photo_id), text, None)
+            request.redirect("/view/" + str(photo_id))
+    else:
+
+        request.write("your not logged in")
+
+
+
