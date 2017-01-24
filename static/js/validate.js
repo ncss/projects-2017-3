66f2a5969c7a79dfd759e9355c5330ebbc0cdb83
jackk <jackywathy24@gmail.com>
2017-01-24 12:40:27 +1100

    refactoring: moved all functions to the top and event handlers to the bottom

--- static/js/validate.js
+++ static/js/validate.js
@@ -1,142 +1,33 @@
-$(document).ready(function(){
+function writeError($input, text){
+    $input.parent().find(".error").text(text)
+}
+function clearError($input){
+    $input.parent().find(".error").text('')
+}
 
-  // The blur event is for when a selector has lost focus
-  $("#username").blur(function(){
-    var $form = $('form.sign-in');
-    var $username = $form.find('#username');
-    var isValidUsername = checkIfPresent($username) && validateName($username);
 
-    $.ajax("/ajax/user_validate", {datatype: "json", type: "post", data: {username: $username.val()},
-        success: function(data){
-            alert(data.user_valid)
-            if(!data.user_valid){
-                 $username.parent()
-                    .find('.error')
-                    .text('This username is already taken!')
-            }
-        }
-        });
-    if (isValidUsername) {
-      return true;
-    } else {
-      return false;
-    }
-  });
-
-  $("#nickname").blur(function(){
-
-    var $form = $('form.sign-in');
-
-    var $nickname = $form.find('#nickname');
-    var isValidNickname =  checkIfPresent($nickname) && validateName($nickname);
-
-    if (isValidNickname) {
-      return true;
-    } else {
-      return false;
-    }
-
-  });
-
-  $("#password").blur(function(){
-
-    var $form = $('form.sign-in');
-
-    var $password = $form.find('#password');
-    var isValidPassword = validatePassword($password);
-
-    if (isValidPassword) {
-      return true;
-    } else {
-      return false;
-    }
-
-  });
-
-  $('#email').blur(function(){
-
-    var $form = $('form.sign-in');
-
-    var $email = $form.find('#email');
-    var isValidEmail = checkIfPresent($email) && validateEmail($email);
-
-    if (isValidEmail) {
-      return true;
-    } else {
-      return false;
-    }
-
-  });
-
-  $('.sign_up_submit').click(function(evt){
-    return validateSignupForm();
-  });
-
-  $('.post_submit').click(function(evt){
-    return validatePost();
-  });
-
-  function checkIfPresent($input) {
-    if ($input.val().length < 4) {
-      $input.parent()
-        .find('.error')
-        .text('At least four characters are required in this field')
-      return false;
-    } else {
-      $input.parent()
-        .find('.error')
-        .text('')
-      return true;
-    }
-  }
-
-  function validateEmail($input) {
+function validateEmail($input) {
     var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
     if (!emailReg.test($input.val())) {
-      $input.parent()
-        .find('.error')
-        .text('This is not a valid email address format')
+      writeError($input, 'This is not a valid email address format')
       return false;
     } else {
-      $input.parent()
-        .find('.error')
-        .text('')
+      clearError($input)
       return true;
     }
   }
 
-  function validateName($input) {
-    var nameReg = /^[A-Za-z0-9]+$/;
-    if (!nameReg.test($input.val())) {
-      $input.parent()
-        .find('.error')
-        .text('Usernames and nicknames can only contain letters and numbers.')
-      return false;
-    } else {
-      $input.parent()
-        .find('.error')
-        .text('')
-      return true;
-    }
+function checkIfPresent($input) {
+  if ($input.val().length < 4) {
+    writeError($input, 'At least four characters are required in this field')
+    return false;
+  } else {
+    clearError($input)
+    return true;
   }
+}
 
-  function validatePassword($input){
-  if ($input.val().length < 8) {
-      $input.parent()
-        .find('.error')
-        .text('At least eight characters are required in this field.')
-      return false;
-    } else {
-      $input.parent()
-        .find('.error')
-        .text('')
-      return true;
-    }
-  }
-
-
-
-  function validateSignupForm() {
+function validateSignupForm() {
     var $form = $('form.sign-in');
     $('.error').html("");
 
@@ -150,24 +41,95 @@ $(document).ready(function(){
     var validPassword = validatePassword($password);
     var validEmail = checkIfPresent($email) && validateEmail($email);
 
-    if (validUsername && validNickname && validPassword && validEmail) {
-      return true;
-    } else {
+    return Boolean(validUsername && validNickname && validPassword && validEmail);
+}
+
+function validateName($input) {
+  var nameReg = /^[A-Za-z0-9]+$/;
+  if (!nameReg.test($input.val())) {
+      writeError($input, 'Usernames and nicknames can only contain letters and numbers.')
       return false;
-    }
+  } else {
+      clearError($input)
+    return true;
   }
+}
 
-  function validatePost() {
-    $('.error').html("");
+function validatePassword($input){
+  if ($input.val().length < 8) {
+      writeError($input, 'At least eight characters are required in this field.')
+      return false;
+    } else {
+      clearError($input)
+      return true;
+  }
+}
 
+function validatePost(){
     var $question = $('.ask_question')
 
     var validQuestion = checkIfPresent($question);
+    return Boolean(validQuestion);
+}
 
-    if (validQuestion) {
-      return true;
-    }else {
-      return false;
-    }
-  }
+
+
+$(document).ready(function(){
+  // The blur event is for when a selector has lost focus
+  $("#username").blur(function(){
+    var $form = $('form.sign-in');
+
+    var $username = $form.find('#username');
+    var isValidUsername = checkIfPresent($username) && validateName($username);
+
+    $.ajax("/ajax/user_validate", {datatype: "json", type: "post", data: {username: $username.val()},
+        success: function(data){
+          if(!data.user_valid){
+            writeError($username, "This username is already taken!");
+            isValidUsername = false;
+          }
+        },
+        failure: function(){
+          alert("failed to ajax");
+        }
+    });
+    return Boolean(isValidUsername);
+    // Boolean converts a value into true or false.
+  });
+
+
+  $("#nickname").blur(function(){
+    var $form = $('form.sign-in');
+    var $nickname = $form.find('#nickname');
+    var isValidNickname =  checkIfPresent($nickname) && validateName($nickname);
+    return Boolean(isValidNickname);
+  });
+
+
+  $("#password").blur(function(){
+    var $form = $('form.sign-in');
+
+    var $password = $form.find('#password');
+    var isValidPassword = validatePassword($password);
+
+    return Boolean(isValidPassword);
+  });
+
+
+  $('#email').blur(function(){
+    var $form = $('form.sign-in');
+
+    var $email = $form.find('#email');
+    var isValidEmail = checkIfPresent($email) && validateEmail($email);
+
+    return Boolean(isValidEmail);
+  });
+
+  $('.sign_up_submit').click(function(evt){
+    return validateSignupForm();
+  });
+
+  $('.post_submit').click(function(evt){
+    return validatePost();
+  });
 });
