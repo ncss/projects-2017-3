43c5e95ac3c5f600cd6db64164453f1620e41758
jackk <jackywathy24@gmail.com>
2017-01-24 22:37:25 +1100

    hotfix ajax error (merges are fun!)

--- back_end/ajax.py
+++ back_end/ajax.py
@@ -1,7 +1,7 @@
 from db import db_api
 
 def username_handler(request):
-    valid = db_api.User.find_by_username(request.get_field("username").lower())
+    valid = db_api.User.find(username=request.get_field("username").lower())
     # checks if the user already signed up
     # if there is no user, it is valid
     valid = True if valid is None else False
