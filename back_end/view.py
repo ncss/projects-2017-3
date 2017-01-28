f09dce42c18495688da0eecc67c5223974cb3309
Tracey Le <traceyle@192-168-1-23.tpgi.com.au>
2017-01-28 13:23:11 +1100

    Removed some console logging that isn't need anymore
    I don’t know about practices/conventions/tips for logging actually ^_^
    but sure was helpful!

--- back_end/view.py
+++ back_end/view.py
@@ -45,16 +45,7 @@ def comment_handler_post(request, photo_id):
             print(user_cookie)
             user = db.User.find(username=user_cookie)
             post = db.Post.find(photo_id)
-            print('LOG: CREATING COMMENT...')
-            print('LOG: photo_id: ', photo_id)
-            print('LOG: post id: ', post.id)
-            print('LOG: text: ', text)
-            print('LOG: user.username', user.username)
             comment = user.create_comment(post, text, None, get_user_picture(user))
-            print('LOG: Created comment.')
-            print('LOG: comment.text: ', comment.text)
-            print('LOG: comment.post: ', comment.post)
-            print('LOG: comment.user.username: ', comment.user.username)
             request.redirect("/view/" + str(photo_id))
     else:
 
