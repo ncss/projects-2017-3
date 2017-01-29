1c63f633d10f7c0ecc6a8fca18853f1809585f01
Jack Jiang <jackywathy24@gmail.com>
2017-01-29 23:22:52 +1100

    added 405 handlers and fixed some stuffs

--- back_end/ajax.py
+++ back_end/ajax.py
@@ -1,4 +1,4 @@
-from db import db_api as db
+from db import db_api
 from back_end.common import reply_malformed, get_secure_username
 
 USER_COOKIE = "current_user"
@@ -6,10 +6,11 @@ USER_COOKIE = "current_user"
 def username_handler(request):
     user = request.get_field("username")
     if isinstance(user, str):
-        valid = db.User.find(username=user.lower())
+        valid = db_api.User.find(username=user.lower())
         # checks if the user already signed up
         # if there is no user, it is valid
         valid = True if valid is None else False
+        print(user, valid)
         request.write({"user_valid" : valid})
     else:
         reply_malformed(request)
@@ -17,7 +18,7 @@ def username_handler(request):
 def email_handler(request):
     email = request.get_field("email")
     if isinstance(email, str):
-        valid = db.User.find(email=email.lower())
+        valid = db_api.User.find(email=email.lower())
         valid = True if valid is None else False
         request.write({"email_valid" : valid})
     else:
@@ -31,3 +32,5 @@ def user_logged_in_handler(request):
     else:
         is_logged_in = False
     request.write({"is_logged_in": is_logged_in})
+
+def username_exists_handler(*args):...
