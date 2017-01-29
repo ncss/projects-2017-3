e71026fec000a70357260452268f8bef3f50d25f
Tracey Le <traceyle@192-168-1-23.tpgi.com.au>
2017-01-29 20:39:11 +1100

    Error message for empty username/password field on sign-in

--- static/js/signin.js
+++ static/js/signin.js
@@ -11,28 +11,70 @@ $(document).ready(function(){
   var $form = $('form.sign-in');
   var $login_button = $('.login_button');
 
+  $('#username').on("change keyup", function(evt){
+
+    var $username = $('#username');
+    var isUsernameEmpty = false
+
+    if ($username.val() == ''){
+      writeError($username, 'This field is required.');
+      isUsernameEmpty = true;
+    } else {
+      clearError($username);
+      isUsernameEmpty = false;
+    }
+
+    return isUsernameEmpty;
+
+  });
+
+  $('#password').on("change keyup", function(evt){
+
+    // Could refactor here and make a function to check if field is empty but yeh this will do for now
+    var $password = $('#password');
+    var isPasswordEmpty = false
+
+    if ($password.val() == ''){
+      writeError($password, 'This field is required.');
+      isPasswordEmpty = true;
+    } else {
+      clearError($password);
+      isPasswordEmpty = false;
+    }
+
+    return isPasswordEmpty;
+
+  });
+
   $('#username').blur(function(evt){
 
     var $username = $('#username');
     var validUsername = false;
 
-    $.ajax("/ajax/signin_username", {
-      async:false,
-      datatype: "json",
-      type: "post",
-      data: {username: $username.val()
-      }, success: function(data){
-          if (data.username_exists){
-            validUsername = true;
-            clearError($username)
-          } else {
-            validUsername = false;
-            writeError($username, "Username cannot be found in database.");
-          }
-      }, failure: function(){
-        alert("Failed to ajax. Check your internet connection.")
-      }
-    });
+    if ($username.val() != ""){
+      $.ajax("/ajax/signin_username", {
+        async:false,
+        datatype: "json",
+        type: "post",
+        data: {username: $username.val()
+        }, success: function(data){
+            if (data.username_exists){
+              validUsername = true;
+              clearError($username)
+              $login_button.attr('disabled', false);
+            } else {
+              validUsername = false;
+              writeError($username, "Username cannot be found in database.");
+              $login_button.attr('disabled', true);
+            }
+        }, failure: function(){
+          alert("Failed to ajax. Check your internet connection.")
+        }
+      });
+    } else {
+      writeError($username, 'This field is required.');
+      validUsername = false;
+    }
 
     return validUsername;
 
